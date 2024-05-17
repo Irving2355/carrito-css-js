0a6482e64fcdb0b55b44f1d26f44a3d740ca4878
Irving2355 <it235597@gmail.com>
2024-05-17 08:40:41 -0600

    17/05/2024

--- main.js
+++ main.js
@@ -28,7 +28,20 @@ function ready(){
     }
     
     /*Casteo del quantity*/
+    var quantityInputs = document.getElementsByClassName("cart-quantity");
+    for(var i=0; i< quantityInputs.length; i++){
+        var input = quantityInputs[i];
+        input.addEventListener("change", quantityChanged);
+    }
     
+}
+
+function quantityChanged(event){
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    updatetotal();
 }
 
 function removeCartItem(event){
@@ -45,13 +58,14 @@ function updatetotal(){
     var total = 0;
     for(var i=0; i< cartBoxes.length; i++){
         var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName("cart-price");
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];/*cambio*/
         var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
         var price = parseFloat(priceElement.innerText.replace("$",""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
+        total = Math.round(total * 100) / 100;
         
-        documet.getElementsByClassName('total-price');
+        document.getElementsByClassName("total-price")[0].innerText = "$" + total;/*cambio*/
     }
 }
 
